0f1d37d9156cb5c9c974792a16f8caad99135ce2
moriya2008 <bill.foolish.heart@gmail.com>
2021-04-14 11:12:04 +0900

    weather_info.php fixed 002

--- weather_info.php
+++ weather_info.php
@@ -7,218 +7,130 @@ require_once __DIR__ . '/common/timezone.php';
 # ページタイトル
 $title = '気象情報';
 
-# 自動更新間隔（10分とする）
+# ブラウザ自動更新間隔（10分とする）
 $reload_interval = 10 * 60;
 
-# getWeather関数（表示形式は現在，都市IDは北見市）
-$response = getWeather('weather', 2129537);
+# 都市名と都市コードの連想配列（新たな都市は都度追加）
+$citycode1 = ['札幌' => 2128295, '東京都' => 1850144, '横浜市' => 1848354, '大阪市' => 1853909];
+$citycode2 = ['名古屋市' => 1856057, '尾道市' => 1853992, '福岡市' => 1863967, '那覇市' => 1856035];
+$citycode3 = ['北見市' => 2129537, '千歳市' => 2130452, '函館市' => 2130188];
 
-#$response = getWeather('weather', 2130741);
-#$response = getWeather('weather', 2128295);
-#$response = getWeather('weather', 2130452);
-#$response = getWeather('weather', 1850147);
+# 上記配列をマージ
+$citycodes = array_merge($citycode1, $citycode2, $citycode3);
 
-# 現在のUNIX時刻（UTC）
-$utc = $response['dt'];
+/*
+気象情報を取得したい都市名を上記連想配列から選択し入力
+今回は北見市とする
+*/
+$city = $citycodes['北見市'];
 
-# DateTimeZoneオブジェクトのインスタンスを生成しJSTを設定
-$jst = new DateTimeZone('Asia/Tokyo');
+# 表示形式と都市コードを指定しJSONを連想配列にデコード
+$response = getAssociative('weather', $city);
 
-/**
- * DateTimeオブジェクトのインスタンスを生成
- * UNIX時刻を日時に変換
- */
-$datetime = new DateTime();
+# 日付の書式1（表示用）
+$date1 = getDatetime($response, 'Y年m月d日');
 
-# UTCをJSTに変換
-$datetime->setTimestamp($utc)->setTimeZone($jst);
+# 日付の書式2（曜日取得用）
+$date2 = getDatetime($response, 'Ymd', 'weather');
 
-# 書式設定し年月日を取得
-$date = $datetime->format('Y年m月d日');
+# 曜日（日本語化）
+$day = getWeek($date2);
 
-# 上記より得られた年月日より曜日を取得する関数
-function getWeek($date) {
+# 時刻の書式
+$time = getDatetime($response, 'H:i');
 
-    # 不要文字を除去するための連想配列
-    $replace = ['年' => '', '月' => '', '日' => ''];
-
-    # 曜日を取得するために変数$dateの不要文字を除去
-    $day = str_replace(array_keys($replace), array_values($replace), $date);
-
-    # 上記より得られた年月日をUNIX時刻に変換
-    $day = strtotime($day);
-
-    # 曜日の配列番号を取得
-    $day = date('w', $day);
-
-    # 曜日の配列
-    $week = ['日', '月', '火', '水', '木', '金', '土'];
-
-    # 連想配列の長さマイナス1
-    $length = count($week) - 1;
-
-    # 曜日の日本語表示の分岐
-    for ($i = 0; $i <= $length; $i++):
-        if ($day == $i):
-            return $week[$i];
-        endif;
-    endfor;
-
-    # null処理
-    $week = null;
-    $length = null;
-}
-
-# 曜日
-$day = getWeek($date);
-
-# 時刻
-$time = $datetime->format('H:i');
-
-# 都市名（英語）
+# 都市名（英語表記）
 $city = $response['name'];
 
-# 都市名を日本語化する関数
-function getCityName($city) {
-
-    # 都市名の連想配列
-    $list = ['Kitami' => '北見市', 'Abashiri' => '網走市', 'Sapporo' => '札幌市', 'Chitose' => '千歳市', 'Tokyo' => '東京都'];
-
-    # 連想配列の長さマイナス1
-    $length = count($list) - 1;
-
-    # 連想配列のキー
-    $keys = array_keys($list);
-
-    # 連想配列の値
-    $values = array_values($list);
-
-    # 都市名の日本語表示の分岐
-    for ($i = 0; $i <= $length; $i++):
-        if ($city == $keys[$i]):
-            return $values[$i];
-        endif;
-    endfor;
-
-    $list = null;
-    $length = null;
-    $keys = null;
-    $values = null;
-}
-
-# 都市名（日本語）
+# 都市名（日本語化）
 $city = getCityName($city);
 
 # 天気情報の配列
 $weather_data = $response['weather'];
 
-# 天気名の連想配列（新たな英語表記の天気名が表示された場合は都度追加）
-$list = ['Clear' => '晴れ', 'Clouds' => 'くもり', 'Rain' => '雨', 'Snow' => '雪', 'Mist' => '霧'];
+# 現在の天気（英語表記）
+$weather = $weather_data[0]['main'];
 
-# 連想配列の長さマイナス1
-$length = count($list) - 1;
+# 現在の天気（日本語化）
+$weather = getWeatherTranslation($weather);
 
-# 連想配列のキー
-$keys = array_keys($list);
+# 天気の詳細（英語表記）
+$description = $weather_data[0]['description'];
 
-# 連想配列の値
-$values = array_values($list);
+# 天気の詳細（日本語化）
+$description = getDescriptionTranslation($description);
 
-# 天気名詳細日本語表示
-foreach ($weather_data as $item):
-
-    # 天気名の日本語表示の分岐
-    for ($i = 0; $i <= $length; $i++):
-        if ($item['main'] == $keys[$i]):
-            $weather = $values[$i];
-        endif;
-    endfor;
-
-    $list = null;
-    $length = null;
-    $keys = null;
-    $values = null;
-
-    # 詳細を関数getTranslationを用いて日本語化
-    $description = getTranslation($item['description']);
-
-    # 天気アイコン
-    $icon = $item['icon'];
-    $img = 'https://openweathermap.org/img/wn/' .$icon .'@2x.png';
-endforeach;
+# 天気アイコン
+$icon = $weather_data[0]['icon'];
+$img = 'https://openweathermap.org/img/wn/' .$icon .'@2x.png';
 
 # 絶対零度
 $absolute_zero = -273.15;
 
-# 天気情報の配列
+# 温度情報のメイン階層
 $temp_data = $response['main'];
 
-# 気温，体感温度，最高気温，最低気温（小数点以下四捨五入摂氏表示）
+# 現在の気温，体感温度，最高気温，最低気温（小数点以下四捨五入かつ摂氏表示）
 $temp = round($temp_data['temp'] + $absolute_zero);
 $feels_like = round($temp_data['feels_like'] + $absolute_zero);
 $temp_max = round($temp_data['temp_max'] + $absolute_zero);
 $temp_min = round($temp_data['temp_min'] + $absolute_zero);
 
-# 気圧
-$pressure = $temp_data['pressure'];
-
 # 湿度
 $humidity = $temp_data['humidity'];
 
-# 風の情報の配列
+# 気圧
+$pressure = $temp_data['pressure'];
+
+# 風の情報のメイン階層
 $wind_data = $response['wind'];
 
 # 風速（小数点第二位で四捨五入）
 $speed = round($wind_data['speed'], 1);
 
-# 風向（関数getDirectionを用いて日本語化)
+# 風向（16方位で日本語化)
 $deg = getDirection($wind_data['deg']);
 
 # DB接続処理
 try {
+
     # DB名
     $dbname = 'weather';
 
-    # 最新の気象更新時間を取得するsql
-    $sql = "select daytime, updated from weather_data order by updated desc limit 1";
+    # 最新の気象更新時間と都市名を取得するSQL
+    $sql = "select datetime, updated from " . $dbname . ".weather_data order by updated desc limit 1";
 
     # DB接続関数
     $stmt = db_connect($dbname, $sql);
 
-    # 最新の気象更新時間を取得
+    # 最新の気象更新日時を取得
     foreach ($stmt as $value):
         $latest = $value[0];
-        $updated = $value[1];
     endforeach;
 
     #null処理
     $stmt = null;
     $sql = null;
 
-    # 年月日DB用書式
-    $date_db = $datetime->format('Y-m-d');
+    # 日時（DB用の書式）
+    $datetime = getDatetime($response, 'Y-m-d H:i:s');
 
-    # 時刻DB用書式
-    $time_db = $datetime->format('H:i:s');
-
-    # 日時DB用書式
-    $daytime_db = $date_db . ' ' . $time_db;
-
-    # エラー日時（協定世界時JST）
-    $unix_error = '1970-01-01 09:00:00';
+    # システム日時
+    $now = date('Y-m-d H:i:s');
 
     /**
-     * ページ読み込み時のエラー回避
-     * 日時がDBに登録されている最新更新日時と異なる場合
-     * かつエラー日時でない場合
-     * かつ最新更新日時より前でない場合はDBに登録
-     */
-    if (($daytime_db != $latest) && ($daytime_db != $unix_error) && ($daytime_db > $latest) && ($daytime_db !=  $updated)):
+      *  〜DB登録条件は下記のいずれか〜
+      * 1. DBに最新の気象更新日時が存在しない場合
+      * 2. 最新の気象更新日時とDBに最新の気象更新日時が異なる場合
+      *    かつ最新の気象更新日時がDBに最新の気象更新日時より後の場合
+      *    かつ日時（DB用の書式）とシステム日時が一致しない場合
+      */
+    if (!isset($latest) || (($datetime != $latest) && ($datetime > $latest) && ($datetime != $now))):
 
         # 気象情報をDBに登録するsql（文字列はシングルクォーテーションで囲むこと）
-        $sql  = "insert into weather_data(daytime, day, city, weather, description, icon, ";
+        $sql  = "insert into " . $dbname . ".weather_data(datetime, day, city, weather, description, icon, ";
         $sql .= "temp, feels_like, temp_max, temp_min, humidity, pressure, speed, deg) ";
-        $sql .= "values('$daytime_db', '$day', '$city', '$weather', '$description', '$icon', ";
+        $sql .= "values('$datetime', '$day', '$city', '$weather', '$description', '$icon', ";
         $sql .= "$temp, $feels_like, $temp_max, $temp_min, $humidity, $pressure, $speed, '$deg')";
 
         # DB接続関数
@@ -226,13 +138,13 @@ try {
 
         # null処理
         $stmt = null;
+        $sql = null;
     endif;
 
 # 例外処理
 } catch (PDOException $e) {
     header('Content-type: text/html; charset=utf-8', true, 500);
-    $msg =  $e->getMessage() . '-' . $e->getLine();
-    redirect($msg);
+    exit('DB接続失敗' . $e->getMessage() . '-' . $e->getLine());
 }
 ?>
 <!doctype html>
@@ -241,25 +153,31 @@ try {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="refresh" content="<?= $reload_interval; ?>">
+    <!--
+    時刻によって背景色と文字色を切り替えるCSSを設定
+    PHPを使用するので拡張子はcssではなくphpにする
+    -->
+    <link rel="stylesheet" href="css/weather_info_css.php">
     <title><?= $title; ?></title>
   </head>
   <body>
-    <h1><?= $title; ?></h1>
-    <div><span><?= $date; ?></span><span><?= '（' . $day . '）' ?></span></div>
-    <div><span><?= $time; ?></span> 時点の気象情報</div>
-    <hr style="border:0;border-top:1px none;">
-    <div>都市名　　: <span><?= $city; ?></span></div>
-    <div>現在の天気: <span><?= $weather; ?></span></div>
-    <div>天気の詳細: <span><?= $description; ?></span></div>
-    <img src="<?= $img; ?>">
-    <div>現在の気温: <span><?= $temp . '℃'; ?></span></div>
-    <div>体感気温　: <span><?= $feels_like . '℃'; ?></span></div>
-    <div>最高気温　: <span><?= $temp_max . '℃'; ?></span></div>
-    <div>最低気温　: <span><?= $temp_min . '℃'; ?></span></div>
-    <div>湿度　　　: <span><?= $humidity . '%'; ?></span></div>
-    <div>気圧　　　: <span><?= $pressure . 'hPa'; ?></span></div>
-    <div>風速　　　: <span><?= $speed . 'm/s'; ?></span></div>
-    <div>風向　　　: <span><?= $deg . 'の風'; ?></span></div>
-    <p><a href="link.php">トップページ</a>へ戻る</p>
+    <font>
+      <h1><?= $title; ?></h1>
+      <div><span><?= $date1; ?></span><span><?= '（' . $day . '）' ?></span></div>
+      <div><span><?= $time; ?></span> 時点の気象情報</div>
+      <hr style="border:0;border-top:1px none;">
+      <div>都市名　　: <span><?= $city; ?></span></div>
+      <div>現在の天気: <span><?= $weather; ?></span></div>
+      <div>天気の詳細: <span><?= $description; ?></span></div>
+      <img src="<?= $img; ?>">
+      <div>現在の気温: <span><?= $temp . '℃'; ?></span></div>
+      <div>体感気温　: <span><?= $feels_like . '℃'; ?></span></div>
+      <div>最高気温　: <span><?= $temp_max . '℃'; ?></span></div>
+      <div>最低気温　: <span><?= $temp_min . '℃'; ?></span></div>
+      <div>湿度　　　: <span><?= $humidity . '%'; ?></span></div>
+      <div>気圧　　　: <span><?= $pressure . 'hPa'; ?></span></div>
+      <div>風速　　　: <span><?= $speed . 'm/s'; ?></span></div>
+      <div>風向　　　: <span><?= $deg . 'の風'; ?></span></div>
+    </font>
   </body> 
 </html>
